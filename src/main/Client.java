cb2e1ed3226d7ca1e7b56966806ef5c325f123d1
andremarko <151890840+andremarko@users.noreply.github.com>
2025-10-14 17:59:03 -0300

    feat(client): implementa logica do client (socket) com comunicacao RSA (keypair)

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,7 +1,44 @@
 package main;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.net.Socket;
+import java.util.Scanner;
 
-public class Client { }
+public class Client {
+    Socket socket;
+    RSA rsa = new RSA();
+    public void communicateWithServer() throws Exception {
+        socket = new Socket("localhost", 9600);
+        Connection conn = new Connection(socket);
+
+        // recebe chave publica do servidor
+        String serverPublicKey = conn.receivePublicKey();
+        System.out.println("Public Key: " + serverPublicKey);
+
+        // envia chave public do client
+        String clientPublicKey = rsa.publicKey();
+        conn.sendPublicKey(clientPublicKey);
+
+        // lê mensagem
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write your message: ");
+        String message = scanner.nextLine();
+
+        String encryptedMessage = rsa.encrypt(message, serverPublicKey);
+        System.out.println("Client sended: " + encryptedMessage);
+        conn.send(encryptedMessage);
+
+        String receivedMessage = conn.receive();
+
+        String decryptedMessage = rsa.decrypt(receivedMessage, rsa.privateKey());
+        System.out.println("Server responded: " + decryptedMessage);
+
+        conn.close();
+    }
+public static void main(String[] args) {
+    try {
+        new Client().communicateWithServer();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+}
